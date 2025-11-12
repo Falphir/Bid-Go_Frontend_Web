8bbfa4a5d76129f983d20270114304576dbf8a4a
Tiago Filipe Teixeira da Costa <8200124@estg.ipp.pt>
2025-11-12 15:04:40 +0000

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,9 @@ class Navbar extends Component {
         return (
             <header className="header">
                 <div className="logo">
+                    <a href={"/"}>
                     <img src={Logo} height={36}/>
+                    </a>
                 </div>
                 <div className="user-info">
                     {/* Placeholder for notifications.  Could show a number of alerts as a badge. */}
